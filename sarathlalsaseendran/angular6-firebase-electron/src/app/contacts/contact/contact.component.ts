43e2a800850945a69d8badcf7190b5cf563932af
CAST D - José Custodio Oliveira Gomes <custodio@saude.gov>
2019-09-06 17:06:14 -0300

    sarathlalsaseendran

---
+++ sarathlalsaseendran/angular6-firebase-electron/src/app/contacts/contact/contact.component.ts
@@ -0,0 +1,55 @@
+import { Component, OnInit } from '@angular/core';
+import { NgForm } from '@angular/forms';
+
+import { ContactService } from '../common/contact.service';
+
+import { ToastrService } from 'ngx-toastr';
+import { dashCaseToCamelCase } from '@angular/animations/browser/src/util';
+
+@Component({
+    selector: 'app-contact',
+    templateUrl: './contact.component.html',
+    styleUrls: ['./contact.component.scss']
+})
+export class ContactComponent implements OnInit {
+
+    constructor(
+        public contactService: ContactService,
+        private toastr: ToastrService
+    ) { }
+
+    ngOnInit() {
+        this.contactService.getData();
+        this.resetForm();
+    }
+
+    onSubmit(contactForm: NgForm) {
+console.log('testrewt');
+
+        if (contactForm.value.$key == null) {
+            this.contactService.insertContact(contactForm.value);
+           // contactForm.created.value=Date();
+            this.toastr.success('Saved successfully', 'Contact added');
+        } else {
+            this.contactService.updateContact(contactForm.value);
+            this.toastr.success('Saved successfully', 'Contact updated');
+        }
+        this.resetForm(contactForm);
+    }
+
+    resetForm(contactForm?: NgForm) {
+        if (contactForm != null) {
+            contactForm.reset();
+        }
+        this.contactService.selectedContact = {
+            $key: null,
+            cpf:'',
+            firstName: '',
+            lastName: '',
+            phone: '',
+            email: '',
+            created:null,
+            modifield:null
+        };
+    }
+}
---
+++ sarathlalsaseendran/angular6-firebase-electron/src/environments/environment.ts
@@ -0,0 +1,91 @@
+export const environment = {
+  production: false,
+
+  firebaseConfig: {
+    apiKey: "AIzaSyCfhmMYgpy6Ux4hiGGNhHnMNeRPQFzS2eM",
+    authDomain: "angular6-firebase-sarath.firebaseapp.com",
+    databaseURL: "https://angular6-firebase-sarath.firebaseio.com",
+    projectId: "angular6-firebase-sarath",
+    storageBucket: "angular6-firebase-sarath.appspot.com",
+    messagingSenderId: "31910280416"
+  },
+
+  firebaseConfig11 : {
+    apiKey: "AIzaSyABb_n8gf_Ao2TQPOBL1TWR9b6HrcofhqQ",
+    authDomain: "pessoas-cad.firebaseapp.com",
+    databaseURL: "https://pessoas-cad.firebaseio.com",
+    projectId: "pessoas-cad",
+    storageBucket: "pessoas-cad.appspot.com",
+    messagingSenderId: "336426390619",
+    appId: "1:336426390619:web:5eca951c14dc2648"
+  }
+
+};
+
+/*
+export const environment = {
+  production: false,
+
+  firebaseConfigOLD: {
+    apiKey: "AIzaSyCfhmMYgpy6Ux4hiGGNhHnMNeRPQFzS2eM",
+    authDomain: "angular6-firebase-sarath.firebaseapp.com",
+    databaseURL: "https://angular6-firebase-sarath.firebaseio.com",
+    projectId: "angular6-firebase-sarath",
+    storageBucket: "angular6-firebase-sarath.appspot.com",
+    messagingSenderId: "31910280416"
+  },
+
+  firebaseConfigMM: {
+    apiKey: "AIzaSyABb_n8gf_Ao2TQPOBL1TWR9b6HrcofhqQ",
+    authDomain: "pessoas-cad.firebaseapp.com",
+    databaseURL: "https://pessoas-cad.firebaseio.com",
+    projectId: "pessoas-cad",
+    storageBucket: "",
+    messagingSenderId: "336426390619",
+    appId: "1:336426390619:web:5b70ab74a47eb775"
+  },
+
+
+  firebaseConfig : {
+    apiKey: "AIzaSyABb_n8gf_Ao2TQPOBL1TWR9b6HrcofhqQ",
+    authDomain: "pessoas-cad.firebaseapp.com",
+    databaseURL: "https://pessoas-cad.firebaseio.com",
+    projectId: "pessoas-cad",
+    storageBucket: "pessoas-cad.appspot.com",
+    messagingSenderId: "336426390619",
+    appId: "1:336426390619:web:b588ab10ad0a4969"
+  }
+  
+}
+
+/*
+
+<script>
+  // Your web app's Firebase configuration
+  var firebaseConfig = {
+    apiKey: "AIzaSyC60YgRSEcMIO7Q7x43LZ3gPS-hXuB_B2M",
+    authDomain: "angular6-firebase-electron.firebaseapp.com",
+    databaseURL: "https://angular6-firebase-electron.firebaseio.com",
+    projectId: "angular6-firebase-electron",
+    storageBucket: "angular6-firebase-electron.appspot.com",
+    messagingSenderId: "862983954997",
+    appId: "1:862983954997:web:55bbb35cea4edf60"
+  };
+  // Initialize Firebase
+  firebase.initializeApp(firebaseConfig);
+
+
+
+  firebaseConfigOLD: {
+    apiKey: "AIzaSyCfhmMYgpy6Ux4hiGGNhHnMNeRPQFzS2eM",
+    authDomain: "angular6-firebase-sarath.firebaseapp.com",
+    databaseURL: "https://angular6-firebase-sarath.firebaseio.com",
+    projectId: "angular6-firebase-sarath",
+    storageBucket: "angular6-firebase-sarath.appspot.com",
+    messagingSenderId: "31910280416"
+  },
+
+
+
+
+*/
